62f3c9251f738e85767701da229058063da21221
Sergio Guoz <sergioguoz1@gmail.com>
2017-02-15 21:10:00 -0600

    Creacion Listas

--- src/practica1s12017_201503925/Practica1s12017_201503925.java
+++ src/practica1s12017_201503925/Practica1s12017_201503925.java
@@ -21,7 +21,7 @@ public class Practica1s12017_201503925 {
         Inicio ini= new Inicio();
         ini.setVisible(true);
         ini.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+        ini.setLocationRelativeTo(null);
 
     }
     
