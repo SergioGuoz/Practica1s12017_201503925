92b0b14a6fa90ee3a3f72fca95090955d1ca0081
SergioGuoz <sergioguoz1@gmail.com>
2017-02-15 20:28:46 -0600

    Add files via upload

---
+++ src/practica1s12017_201503925/Practica1s12017_201503925.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1s12017_201503925;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Sergio
+ */
+public class Practica1s12017_201503925 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Inicio ini= new Inicio();
+        ini.setVisible(true);
+        ini.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ini.setLocationRelativeTo(null);
+
+    }
+    
+}
